9a9dbd6282ac51d5d56e10f7305da362609fa254
Erik Zakrzewski and Kristen Kulha <kristen@>
2017-09-05 13:39:01 -0700

    add spec 4 functionality

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,6 +2,7 @@ class String
   def leetspeak
     self.gsub!("e", "3")
     self.gsub!("t", "7")
+    self.gsub!("o", "0")
     self
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -11,4 +11,7 @@ describe("#leetspeak") do
   it('replaces every "t" in a string with a "7"') do
     expect(("elephant").leetspeak()).to(eq("3l3phan7"))
   end
+  it('replaces every "o" in a string with a "0"') do
+    expect(("boo").leetspeak()).to(eq("b00"))
+  end
 end
