3b9a93c2d135d09bde5205bee7df1b8cb0e0f3e6
Erik Zakrzewski and Kristen Kulha <kristen@>
2017-09-05 14:49:51 -0700

    add last spec functionality

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,12 +1,19 @@
 class String
   def leetspeak
-    self.gsub!("e", "3")
-    self.gsub!("t", "7")
-    self.gsub!("o", "0")
-    self.gsub!("i", "1")
-    self.gsub!("I", "1")
-    self.gsub!("s", "5")
-    self.gsub!("S", "5")
-    self
+    sentence_words = self.split
+    sentence_words.each do |word|
+      word.gsub!("s", "5")
+      word.gsub!("S", "5")
+      word.gsub!("e", "3")
+      word.gsub!("t", "7")
+      word.gsub!("o", "0")
+      word.gsub!("i", "1")
+      word.gsub!("I", "1")
+      if(word[0] == "5")
+        word[0] = ''
+        word.prepend("s")
+      end
+    end
+    sentence_words.join(" ")
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -18,6 +18,9 @@ describe("#leetspeak") do
     expect(("I like dog").leetspeak()).to(eq("1 l1k3 d0g"))
   end
   it('replaces every "s" in a string with a "5"') do
-    expect(("stop").leetspeak()).to(eq("570p"))
+    expect(("likes").leetspeak()).to(eq("l1k35"))
+  end
+  it('replaces letters correctly in a string of words') do
+    expect(("I scream you scream we all scream for raspberry ice cream").leetspeak()).to(eq("1 scr3am y0u scr3am w3 all scr3am f0r ra5pb3rry 1c3 cr3am"))
   end
 end
